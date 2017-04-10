821b0318b646b0c54b8ee3a42a278cec840e677b
Domeniqque Dylluar <domeniqque@hotmail.com>
2017-04-10 16:19:13 -0400

    adicionado standard js

--- prototypes.js
+++ prototypes.js
@@ -1,18 +1,19 @@
 
-function Person(name, year) {
-    this.name = name;
-    this.year = year || 1;
-
-    this.speak = function (words) {
-        console.log(this.name + ', de ' + this.year + ' anos, falou "' + words + '".');
-    };
-
-    this.age = function (years) {
-        this.year += years || 1;
-        console.log(this);
-    };
+function Person (name, year) {
+  this.name = name
+  this.year = year || 1
 }
 
-const domeniqque = new Person('Domeniqque Dylluar', 20);
+Person.prototype.speak = function (words) {
+  console.log(this.name + ', de ' + this.year + ' anos, falou "' + words + '".')
+}
 
-console.log(domeniqque);
+Person.prototype.age = function (years) {
+  this.year += years || 1
+  console.log(this)
+}
+
+const domeniqque = new Person('Domeniqque Dylluar', 20)
+
+console.log(domeniqque)
+domeniqque.speak('Olá mundo!!')
